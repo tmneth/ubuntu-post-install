e012d3635f2b694bf4530ba5f7f32a9c389ed7fc
Maksim Barbašov <80415416+tmneth@users.noreply.github.com>
2022-01-12 21:17:25 +0200

    Added curl & gh installation

--- post-install.sh
+++ post-install.sh
@@ -56,6 +56,13 @@ flatpak install flathub  org.gimp.GIMP -y
 echo "Installing OBS"
 flatpak install flathub  com.obsproject.Studio -y
 
+sudo apt install curl
+# Installing gh
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
 sudo apt install dialog
 cmd=(dialog --separate-output --checklist "Please Select Optional Software you want to install:" 114 137 218)
 options=(
