fe00d2338ec2d8d673f5853d4c3d20ab65f4a095
Maksim Barbašov <80415416+tmneth@users.noreply.github.com>
2022-01-12 19:38:28 +0200

    Uninstall default apps

--- post-install.sh
+++ post-install.sh
@@ -8,3 +8,9 @@ echo "Installing Flatpak"
 sudo apt install flatpak -y
 flatpak update -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Uninstall Default Ubuntu Apps
+sudo apt purge aisleriot -y
+sudo apt purge gnome-mahjongg -y
+sudo apt purge gnome-mines -y
+sudo apt purge gnome-sudoku -y
