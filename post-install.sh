024de799239ab9fbdd8094f4721a0d2a1ebe506f
Maksim Barbašov <80415416+tmneth@users.noreply.github.com>
2022-01-12 19:39:44 +0200

    Using flatpak install applications

--- post-install.sh
+++ post-install.sh
@@ -14,3 +14,44 @@ sudo apt purge aisleriot -y
 sudo apt purge gnome-mahjongg -y
 sudo apt purge gnome-mines -y
 sudo apt purge gnome-sudoku -y
+
+
+#VS Code
+echo "Installing Visual Studio Code"
+flatpak install flathub com.visualstudio.code -y
+
+#Node.js
+echo "Installing Node.js"
+sudo apt install nodejs -y
+
+#NPM
+echo "Installing Npm"
+sudo apt install npm -y
+
+#GIT
+echo "Installing Git"
+sudo apt install git -y
+
+#Chromium
+echo "Installing Chromium"
+flatpak install flathub com.github.Eloston.UngoogledChromium -y
+
+#Discord
+echo "Installing Discord"
+flatpak install flathub com.discordapp.Discord -y
+
+#Telegram
+echo "Installing Telegram"
+flatpak install flathub org.telegram.desktop -y
+
+#OnlyOffice
+echo "Installing OnlyOffice"
+flatpak install flathub  org.onlyoffice.desktopeditors -y
+
+#GIMP
+echo "Installing GIMP"
+flatpak install flathub  org.gimp.GIMP -y
+
+#OBS
+echo "Installing OBS"
+flatpak install flathub  com.obsproject.Studio -y
