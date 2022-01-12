0e7380d65b4753424b34f5722152a777605f01f5
Maksim Barbašov <80415416+tmneth@users.noreply.github.com>
2022-01-12 19:37:21 +0200

    Added the Flathub repository

--- post-install.sh
+++ post-install.sh
@@ -7,3 +7,4 @@ sudo apt update && sudo apt upgrade -y
 echo "Installing Flatpak"
 sudo apt install flatpak -y
 flatpak update -y
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
