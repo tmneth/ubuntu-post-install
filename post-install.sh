e8fbe162de90edf6b56b7c5749d3afb8a2b7bc79
Maksim Barbašov <80415416+tmneth@users.noreply.github.com>
2022-01-13 21:05:58 +0200

    Added neofetch install script

--- post-install.sh
+++ post-install.sh
@@ -76,11 +76,15 @@ echo "Installing OBS"
 flatpak install flathub  com.obsproject.Studio -y
 
 sudo apt install curl
-# Installing gh
+#Installing gh
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
 sudo apt install gh
+
+#Neofetch
+sudo apt install neofetch -y
+
 
 sudo apt install dialog
 cmd=(dialog --separate-output --checklist "Please Select Optional Software you want to install:" 114 137 218)
